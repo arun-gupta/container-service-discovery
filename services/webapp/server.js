80e8141ff6fe30959e61a0e2dafb82dafd7317da
Arun Gupta <arun.gupta@gmail.com>
2017-08-02 09:23:09 -0700

    using parameters for service host/port

--- services/webapp/server.js
+++ services/webapp/server.js
@@ -11,20 +11,18 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  var greet = req.query['greet'];
-  var id = req.query['id'];
+    var args = process.argv.slice(2);
 
-  console.log('greet: ' + greet);
-  console.log('id: ' + id);
+    var greeter = "http://" + args[0] + ":" + args[1] + "?greet=" + req.query['greet'];
+    var name = "http://" + args[2] + ":" + args[3] + "?id=" + req.query['id'];
+    console.log('greeter: ' + greeter);
+    console.log('name: ' + name);
 
-  res.send(get("http://greeter:8080?greet=" + greet)
-  	+ ' '  
-  	+ get("http://name:8080?id=" + id));
+    res.send(get(greeter) + ' ' + get(name));
 });
 
 function get(url) {
 	return require('sync-request')('GET', url).getBody();
-
 }
 
 app.listen(PORT, HOST);
